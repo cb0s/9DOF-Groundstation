4a348bcef8281117afba161aeb30a434a8da9c19
Ludwig Richter <riluzm@posteo.de>
2021-05-28 22:53:32 +0200

    feat(widget): Import placeholder widget

--- client/src/widgets/index.ts
+++ client/src/widgets/index.ts
@@ -9,10 +9,12 @@ import { Widget } from '@wuespace/telestion-client-types';
 import { widget as sampleWidget } from './sample-widget';
 import { widget as currentValuesWidget } from './current-values-widget';
 import { widget as pgraphWidget } from './pgraph-widget';
+import { widget as placeholderWidget } from './placeholder-widget';
 // IMPORT_INSERT_MARK
 
 export const projectWidgets: Widget[] = [
 	// ARRAY_FIRST_ELEMENT_INSERT_MARK
+	placeholderWidget,
 	pgraphWidget as Widget,
 	currentValuesWidget as Widget,
 	sampleWidget
