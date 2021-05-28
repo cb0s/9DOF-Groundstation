eb1c358c85855c195166f5091f91149c0269d951
Ludwig Richter <riluzm@posteo.de>
2021-05-28 22:52:52 +0200

    feat(widget): Import pgraph widget

--- client/src/widgets/index.ts
+++ client/src/widgets/index.ts
@@ -8,10 +8,12 @@ using the tc-cli generate widget command.
 import { Widget } from '@wuespace/telestion-client-types';
 import { widget as sampleWidget } from './sample-widget';
 import { widget as currentValuesWidget } from './current-values-widget';
+import { widget as pgraphWidget } from './pgraph-widget';
 // IMPORT_INSERT_MARK
 
 export const projectWidgets: Widget[] = [
 	// ARRAY_FIRST_ELEMENT_INSERT_MARK
+	pgraphWidget as Widget,
 	currentValuesWidget as Widget,
 	sampleWidget
 ];
