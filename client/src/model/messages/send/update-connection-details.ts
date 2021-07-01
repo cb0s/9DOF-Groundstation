0a6496cfee2c219028df7813648cc598827536eb
Ludwig Richter <riluzm@posteo.de>
2021-07-01 12:12:10 +0200

    style(client): Fix styling issues

--- client/src/model/messages/send/update-connection-details.ts
+++ client/src/model/messages/send/update-connection-details.ts
@@ -1,7 +1,8 @@
 import { JsonSerializable } from '@wuespace/telestion-client-types';
 import { ClassNamePrefix } from '../../channels';
 
-export interface UpdateConnectionDetails extends Record<string, JsonSerializable> {
+export interface UpdateConnectionDetails
+	extends Record<string, JsonSerializable> {
 	device: string;
 	baudrate: number;
 	className: `${ClassNamePrefix}.UpdateConnectionDetails`;
