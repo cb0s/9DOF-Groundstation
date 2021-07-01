472dd90376c62680c184b53c6df2bcd8a10c59f0
Ludwig Richter <riluzm@posteo.de>
2021-07-01 12:11:32 +0200

    fix(client): Fix breaking changes

--- client/src/model/dashboards/overview.ts
+++ client/src/model/dashboards/overview.ts
@@ -6,52 +6,52 @@ export const Overview: Dashboard = {
 	rows: 12,
 	widgets: [
 		{
+			id: 'overview-1',
 			widgetName: 'sampleWidget',
 			width: 4,
-			height: 4,
-			title: 'Widget 1'
+			height: 4
 		},
 		{
+			id: 'overview-2',
 			widgetName: 'sampleWidget',
 			width: 6,
-			height: 4,
-			title: 'Widget 1'
+			height: 4
 		},
 		{
+			id: 'overview-3',
 			widgetName: 'sampleWidget',
 			width: 2,
-			height: 4,
-			title: 'Widget 1'
+			height: 4
 		},
 		{
+			id: 'overview-4',
 			widgetName: 'sampleWidget',
 			width: 6,
-			height: 4,
-			title: 'Widget 1'
+			height: 4
 		},
 		{
+			id: 'overview-5',
 			widgetName: 'sampleWidget',
 			width: 6,
-			height: 4,
-			title: 'Widget 1'
+			height: 4
 		},
 		{
+			id: 'overview-6',
 			widgetName: 'sampleWidget',
 			width: 3,
-			height: 4,
-			title: 'Widget 1'
+			height: 4
 		},
 		{
+			id: 'overview-7',
 			widgetName: 'sampleWidget',
 			width: 5,
-			height: 4,
-			title: 'Widget 1'
+			height: 4
 		},
 		{
+			id: 'overview-8',
 			widgetName: 'sampleWidget',
 			width: 4,
-			height: 4,
-			title: 'Widget 1'
+			height: 4
 		}
 	]
 };
